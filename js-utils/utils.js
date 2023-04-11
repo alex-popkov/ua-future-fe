3c07ee2eb592d0a00e76d13627cead8302bcc000
Oleksandr.Popkov <opopkov@deloitte.com>
2023-04-11 16:47:22 +0300

    added a scroller for the teammates. added a jquery file to the project

--- js-utils/utils.js
+++ js-utils/utils.js
@@ -10,3 +10,49 @@ function copyToClipboard(id) {
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+//teammate scrolling
+$(document).ready(function() {
+    var scrolled = 0;
+    var duration = 500;
+    var teammates = $("#teammates");
+    var scrollNav = $("#ua-scroll-nav");
+    var teammatesOffsetWidth = teammates.width();
+    var teammatesScrollWidth = teammates[0].scrollWidth;
+    var buffer = teammatesScrollWidth - teammatesOffsetWidth;
+
+    var changeScrollNavVisibilityState = function (widthBuffer, navElement){
+        if (widthBuffer === 0) {
+            navElement.hide();
+        } else {
+            navElement.show();
+        }
+    }
+
+    changeScrollNavVisibilityState(buffer, scrollNav);
+
+    $(window).resize(function() {
+        teammatesOffsetWidth = teammates.width();
+        teammatesScrollWidth = teammates[0].scrollWidth;
+        buffer = teammatesScrollWidth - teammatesOffsetWidth;
+        changeScrollNavVisibilityState(buffer, scrollNav);
+    });
+
+    $("#ua-scroll-next").click(function() {
+        if (scrolled <= buffer) {
+            scrolled = scrolled + 138;
+            teammates.animate({
+                scrollLeft: scrolled
+            }, duration);
+        }
+    });
+    $("#ua-scroll-prev").click(function() {
+        if (scrolled > 0) {
+            scrolled = scrolled - 138;
+            teammates.animate({
+                scrollLeft: scrolled
+            }, duration);
+        }
+    });
+});
+
